363b5814b068d70ffc5ea7829f9a826b1df43bd0
Udit agarwal <uditagarwal1997@gmail.com>
2017-08-24 04:17:33 +0530

    Updated scripts

--- duplicacy_removal.py
+++ duplicacy_removal.py
@@ -6,24 +6,29 @@ Created on Mon Aug 21 23:38:14 2017
 """
 
 import csv,sys
-f = open("SMP 2017 Student Form (Responses) - Sheet2.csv","rb")
-f2=open("sorted.txt","w")
+f = open("FInal_allocationSMP_2017 - Sheet1.csv","rb")
+
 csv.field_size_limit(sys.maxint)
 reader = csv.reader(f)
 list1_a=[]
 
 for row in reader:
-    if row[0] != '' and row[0] !=' ':
-        list1_a.append(row[0])
+    if row[6] != '' and row[6] !=' ':
+        list1_a.append(row[6].lower())
         print "appended"
     pass
-f = open("SMP 2017 Student Form (Responses) - Sheet2.csv","rb")
+print list1_a
+f = open("SMP Mentor Responses - Form Responses 1.csv","rb")
 reader = csv.reader(f)
 #print list1_a
+count =0
 for row1 in reader:  
     #print "Hello"
-    try:
-        a= list1_a.index(row1[1])
-        #print "Hello"
-    except:
-        print row1[1]
+    if row[1] != '' and row[1] !=' ':
+        try:
+            a= list1_a.index(row1[1].lower())
+            #print "Hello"
+        except:
+            count = count + 1
+            print row1[0]+","+row1[1].upper()+","+row1[2]+","+row1[3]+","+row1[4]+","+row1[5]+","+row1[6]+","+row1[7]
+print count
